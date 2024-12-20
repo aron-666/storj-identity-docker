ccab78652287112a6a2c6dc77d372bde9dae3653
Aron <aronkao0115@gmail.com>
2024-12-20 12:00:41 +0800

    update

--- app/run.sh
+++ app/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -d /root/.local/share/storj/identity/storagenode ]; then
+    echo "storagenode folder exists"
+    exit 1
+fi
+
 curl -L https://github.com/storj/storj/releases/latest/download/identity_linux_amd64.zip -o identity_linux_amd64.zip
 
 unzip -o identity_linux_amd64.zip
@@ -13,13 +18,13 @@ chmod +x identity
 ./identity create storagenode $EMAIL_TOKEN
 
 # check grep -c BEGIN ~/.local/share/storj/identity/storagenode/ca.cert is 2
-if [ $(grep -c BEGIN ~/.local/share/storj/identity/storagenode/ca.cert) -ne 2 ]; then
+if [ $(grep -c BEGIN /root/.local/share/storj/identity/storagenode/ca.cert) -ne 2 ]; then
     echo "ca.cert is invalid"
     exit 1
 fi
 
 # check grep -c BEGIN ~/.local/share/storj/identity/storagenode/identity.cert is 3
-if [ $(grep -c BEGIN ~/.local/share/storj/identity/storagenode/identity.cert) -ne 3 ]; then
+if [ $(grep -c BEGIN /root/.local/share/storj/identity/storagenode/identity.cert) -ne 3 ]; then
     echo "identity.cert is invalid"
     exit 1
 fi
